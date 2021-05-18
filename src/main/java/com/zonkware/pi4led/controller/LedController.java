c3ef54e61421d96bf2086c6483f17594e3fd1b43
Machado Junior <b.machado.junior@accenture.com>
2021-05-18 18:55:46 -0300

    ajuste

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -14,7 +14,7 @@ public class LedController {
         return "Hello world!";
     }
 
-    @RequestMapping("/luz")
+    @RequestMapping("/light")
     public String light() {
         if (pin == null) {
             GpioController gpio = GpioFactory.getInstance();
