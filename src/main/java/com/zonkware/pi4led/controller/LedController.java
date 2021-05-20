9dca7bc147c9d63f074bf97a7508bbf1380f7be4
Machado Junior <b.machado.junior@accenture.com>
2021-05-20 19:07:17 -0300

    1.2

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -1,8 +1,13 @@
 package com.zonkware.pi4led.controller;
 
-import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
 
 @RestController
 public class LedController {
